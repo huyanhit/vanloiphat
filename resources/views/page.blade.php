12279e229f2dfe3228bb6e233e9097a5013fae9c
huy le <huyanhit@gmail.com>
2024-04-06 10:27:40 +0700

    update

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -18,21 +18,21 @@
                     <h1 class="px-2 block w-full text-center font-bold text-xl">
                         <span class="ml-2 text-center text-uppercase">{!! $pages->title !!}</span>
                     </h1>
-                    <div class="description p-3 border-1 inline-block mt-2 bg-gray-100 font-italic">
+                    <div class="description p-3 border-1 inline-block mt-2 bg-gray-50 font-semibold">
                         {!! $pages->description !!}
                     </div>
-                    <article class="my-3 p-3 prose prose-sm prose-p:m-0">
+                    <article class="my-3 p-3 prose prose-sm prose-p:m-0 border-1 bg-gray-100">
                         {!! $pages->content !!}
                     </article >
 
                     <div class="my-3 text-center">
-                        <a class="px-2 py-2 bg-cyan-700 hover:bg-cyan-900 text-white rounded text-decoration-none" href="/">Quay lại trang chủ</a>
+                        <a class="px-2 py-2 bg-gray-700 hover:bg-gray-900 text-white rounded text-decoration-none" href="/">Quay lại trang chủ</a>
                     </div>
                 @else
                     <article class="my-1 p-3 prose prose-sm prose-p:m-0 text-center text-uppercase">
                         Trang đang hoàn thiện. Cảm ơn quý khách đã ghé thăm.
                         <div class="my-3">
-                            <a class="px-2 py-2 bg-cyan-700 hover:bg-cyan-900 text-white rounded text-decoration-none" href="/">Quay lại trang chủ</a>
+                            <a class="px-2 py-2 bg-gray-700 hover:bg-gray-900 text-white rounded text-decoration-none" href="/">Quay lại trang chủ</a>
                         </div>
                     </article >
                 @endif
