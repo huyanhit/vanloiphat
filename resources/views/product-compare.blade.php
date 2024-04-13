94165de3c94b1766e54b81909a78f1c95808b385
huy le <huyanhit@gmail.com>
2024-04-13 11:47:20 +0700

    update

--- resources/views/product-compare.blade.php
+++ resources/views/product-compare.blade.php
@@ -193,14 +193,14 @@
                     </li>
                 </ul>
                 <div class="tab-content" id="myTabContent">
-                    <div class="tab-pane fade show active" id="ct-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-                        <article class="p-3 prose prose-sm prose-p:m-0 bg-white border-1 border-top-0">
-                            {!! $product->content !!}
-                        </article >
-                    </div>
                     <div class="tab-pane fade" id="ts-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
                         <article class="p-3 prose prose-sm prose-p:m-0 bg-white border-1 border-top-0">
                             {!! $product->description !!}
+                        </article >
+                    </div>
+                    <div class="tab-pane fade show active" id="ct-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                        <article class="p-3 prose prose-sm prose-p:m-0 bg-white border-1 border-top-0">
+                            {!! $product->content !!}
                         </article >
                     </div>
                     <div class="tab-pane fade" id="bh-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
@@ -383,14 +383,14 @@
                     </li>
                 </ul>
                 <div class="tab-content" id="myTabContent2">
-                    <div class="tab-pane fade show active" id="ct-tab-pane2" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-                        <article class="p-3 prose prose-sm prose-p:m-0 bg-white border-1 border-top-0">
-                            {!! $product2->content !!}
-                        </article >
-                    </div>
                     <div class="tab-pane fade" id="ts-tab-pane2" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
                         <article class="p-3 prose prose-sm prose-p:m-0 bg-white border-1 border-top-0">
                             {!! $product2->description !!}
+                        </article >
+                    </div>
+                    <div class="tab-pane fade show active" id="ct-tab-pane2" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                        <article class="p-3 prose prose-sm prose-p:m-0 bg-white border-1 border-top-0">
+                            {!! $product2->content !!}
                         </article >
                     </div>
                     <div class="tab-pane fade" id="bh-tab-pane2" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
